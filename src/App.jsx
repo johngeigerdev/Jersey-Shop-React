b940e98f2f5b130aa56750895cf255ec2aa4a31d
John <jgeiger@Mac.lan>
2025-08-22 07:19:06 -0400

    props added to items array and app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,94 @@
 import './App.css';
 import OrderDetails from './components/OrderDetails';
 import Item from './components/Item';
+import {useState} from 'react';
 
 function App() {
-    
-    const items = [
-        {
-            
-            id: 1, 
-            photo: "real_madrid.webp",
-            name: "Real Madrid",
-            price: 119.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 2, 
-            photo: "milan.png",
-            name: "Milan",
-            price: 99.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 3, 
-            photo: "chelsea.webp",
-            name: "Chelsea",
-            price: 99.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 4, 
-            photo: "barcelona.png",
-            name: "Barcelona",
-            price: 109.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 5, photo: "benfica.png",
-            name: "Benfica",
-            price: 89.49,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 6, 
-            photo: "manchester.webp",
-            name: "Manchester City",
-            price: 129.79,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 7, 
-            photo: "bayern.webp",
-            name: "Bayern",
-            price: 119.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 8, 
-            photo: "psg.png",
-            name: "PSG",
-            price: 94.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        },
-        {
-            id: 9, 
-            photo: "ajax.webp",
-            name: "Ajax",
-            price: 89.99,
-            active: false,
-            quantity: 1, 
-            isInBag: false
-        }
-    ];
+    const [items, setItems] = useState(
+        [
+            {
+                
+                id: 1, 
+                photo: "real_madrid.webp",
+                name: "Real Madrid",
+                price: 119.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 2, 
+                photo: "milan.png",
+                name: "Milan",
+                price: 99.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 3, 
+                photo: "chelsea.webp",
+                name: "Chelsea",
+                price: 99.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 4, 
+                photo: "barcelona.png",
+                name: "Barcelona",
+                price: 109.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 5, photo: "benfica.png",
+                name: "Benfica",
+                price: 89.49,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 6, 
+                photo: "manchester.webp",
+                name: "Manchester City",
+                price: 129.79,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 7, 
+                photo: "bayern.webp",
+                name: "Bayern",
+                price: 119.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 8, 
+                photo: "psg.png",
+                name: "PSG",
+                price: 94.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            },
+            {
+                id: 9, 
+                photo: "ajax.webp",
+                name: "Ajax",
+                price: 89.99,
+                active: false,
+                quantity: 1, 
+                isInBag: false
+            }
+        ]
+    );
 
     const itemsInBag = items.filter(item => item.isInBag);
     console.log(itemsInBag);
@@ -99,7 +101,7 @@ function App() {
                 {/* must always add a key for each Item when mapping through a list as done below */}
                 {items.map(item => 
                     <Item 
-                        selectProduct = {(id) => alert(`clicked product ${id}`)}
+                        selectProduct = {(id) => setItems([])}
                         item={item} 
                         key={item.id} 
 
